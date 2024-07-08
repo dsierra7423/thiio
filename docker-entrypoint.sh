9a0cccb6a836deddeb88f53cfdf75e002ecc5dca
dsierra-volindo <dsierra@volindo.com>
2024-07-07 22:53:10 -0600

    First Version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+DB_HOST=${DB_HOST:-localhost}
+DB_PORT=${DB_PORT:-3306}
+DB_USERNAME=${DB_USERNAME:-root}
+DB_PASSWORD=${DB_PASSWORD:-password}
+DB_DATABASE=${DB_DATABASE:-laravel}
+
+
 # Wait for MySQL to be ready
-while ! mysqladmin ping -h db --silent; do
-    echo "Waiting for database connection..."
-    sleep 2
+echo "Waiting for MySQL to be ready..."
+until mysql -h "${DB_HOST}" -P "${DB_PORT}" -u "${DB_USERNAME}" -p"${DB_PASSWORD}" -e "SHOW DATABASES;" > /dev/null 2>&1; do
+  echo "MySQL is unavailable - sleeping"
+  sleep 3
 done
+echo "MySQL is up - executing command"
+
 
 # Run migrations and seeds
 php artisan migrate --force
