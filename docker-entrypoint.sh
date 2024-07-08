08d00ad4ca153181e6cc062aacc9a732e5540338
dsierra-volindo <dsierra@volindo.com>
2024-07-07 22:29:25 -0600

    First Version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,16 +2,3 @@
 
 composer install 
 php artisan key:generate
-
-# Wait for MySQL to be ready
-while ! mysqladmin ping -h db --silent; do
-    echo "Waiting for database connection..."
-    sleep 2
-done
-
-# Run migrations and seeds
-php artisan migrate --force
-php artisan db:seed --force
-
-# Start PHP-FPM
-exec php-fpm
